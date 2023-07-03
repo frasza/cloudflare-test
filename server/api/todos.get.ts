d7df11eced4a7b62f0536bb684676182cf41cb8b
frasza <zan.fraas@gmail.com>
2023-07-03 14:26:35 +0200

    Fixes and api docs

--- server/api/todos.get.ts
+++ server/api/todos.get.ts
@@ -1,19 +1,25 @@
+interface Todo {
+  id: number
+  title: string
+  complete: boolean
+}
+
 export default defineEventHandler((event) => {
   return [
     {
       id: 1,
       title: 'Todo 1',
-      complete: false
+      complete: false,
     },
     {
       id: 2,
       title: 'Todo 2',
-      complete: false
+      complete: false,
     },
     {
       id: 3,
       title: 'Todo 3',
-      complete: true
-    }
-  ]
+      complete: true,
+    },
+  ] as Array<Todo>
 })
